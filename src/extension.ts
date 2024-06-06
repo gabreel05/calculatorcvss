00d77072f7c9428081059446a7744344185a118e
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2024-06-05 23:10:52 -0300

    Adding WebViewPanel

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "calculatorcvss" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
     'calculatorcvss.calculateCVSS',
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
       vscode.window.showInformationMessage('Calculate CVSS!');
       vscode.window.showInputBox({
         placeHolder: 'Enter CVSS string',
@@ -57,18 +42,13 @@ export function activate(context: vscode.ExtensionContext) {
                   <title>Document</title>
                 </head>
                 <body>
-                  <h1>CVSS Base Score</h1>
-                  <p>The CVSS base score is: ${baseScore}</p>
+                  <div class="main" data-vscode-context='{"webviewSection": "main", "mouseCount": 4}'>
+                    <h1>CVSS Base Score</h1>
+                    <p>The CVSS base score is: ${baseScore}</p>
+                  </div>
                 </body>
                 </html>`;
-              vscode.commands.executeCommand(
-                'vscode.open',
-                vscode.Uri.parse(
-                  `http://services.nvd.nist.gov/rest/json/cves/2.0?cvssV3Metrics=${text}`
-                )
-              );
 
-              // vscode.window.showTextDocument(vscode.Uri.parse(baseScore));
               vscode.window.showInformationMessage(
                 `The CVSS base score is: ${baseScore}`
               );
@@ -83,5 +63,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
